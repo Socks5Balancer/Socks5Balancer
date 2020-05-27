49adc5d2a9d74b027e0615584da532c1b8e2c599
Jeremie <lucheng989898@gmail.com>
2020-05-27 13:34:54 +0800

    doing upstream pool

--- src/index.ts
+++ src/index.ts
@@ -16,57 +16,10 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-// https://stackoverflow.com/questions/24857127/redirecting-clients-from-one-node-js-server-to-another-node-js-server
-import net from 'net';
-import bluebird from 'bluebird';
-import {globalConfig} from './configLoader';
-import {getServerBasedOnAddress} from './upstreamPool';
+import {initUpstreamPool} from './upstreamPool';
+import {initServer} from './server';
 
-// Create the proxy server
-net.createServer(async (socket: net.Socket) => {
 
-  for (let i = 0; i < globalConfig.get('retryTimes', 3); ++i) {
-    // retry 3 times
-    try {
-      // get a server
-      const upstream = getServerBasedOnAddress(socket.remoteAddress);
 
-      // try to connect
-      const p = new bluebird<net.Socket>((resolve, reject) => {
-        try {
-          const _s = net.createConnection(upstream.port, upstream.host, () => {
-            resolve(_s);
-          });
-        } catch (e) {
-          reject(e);
-        }
-      });
-      // if timeout, will throw a bluebird.TimeoutError
-      // http://bluebirdjs.com/docs/api/timeout.html
-      await p.timeout(globalConfig.get('connectTimeout', 2 * 1000)).then(s => {
-        // if ok, connect each other
-        socket.pipe(s);
-        s.pipe(socket);
-        console.log(`connected to ${upstream.host}:${upstream.port}`);
-      });
-      // if no error, dont retry and break the for-loop
-      break;
-    } catch (e) {
-      // timeout , retry it
-    }
-  }
-
-  // retry failed
-  socket.end();
-  socket.destroy(new Error('Cannot find valid upstream.'));
-
-}).listen(
-  globalConfig.get('listenPort', 5000),
-  globalConfig.get('listenHost', '127.0.0.1'),
-  () => {
-    console.log(`Ready to proxy data, ` +
-      `listenPort:${globalConfig.get('listenPort', 5000)} ` +
-      `listenHost:${globalConfig.get('listenHost', '127.0.0.1')}`
-    );
-  }
-);
+initUpstreamPool();
+initServer();
--- src/upstreamPool.ts
+++ src/upstreamPool.ts
@@ -18,15 +18,36 @@
 
 import {testSocks5} from './BackendTester';
 import {globalConfig} from './configLoader';
+import {Subscription, timer} from 'rxjs';
+import moment from 'moment';
+import {assign} from 'lodash'
 
 testSocks5();
 
+interface UpstreamInfo {
+  host: string;
+  port: number;
+  lastOnline: moment.Moment;
+  lastConnectAble: moment.Moment;
+}
+
+const defaultUpstreamInfo: Omit<UpstreamInfo, 'host' | 'port'> = {
+  lastOnline: moment(),
+  lastConnectAble: moment(),
+};
+
 // The servers we will proxy to
-const upstreamServerAddresses: { host: string, port: number }[] = globalConfig.get('upstream', [
-  {host: '127.0.0.1', port: 3000},
-  {host: '127.0.0.1', port: 3001},
-  {host: '127.0.0.1', port: 3002},
-]);
+let upstreamServerAddresses: UpstreamInfo[] = [];
+
+export function initUpstreamPool() {
+  upstreamServerAddresses = globalConfig.get('upstream', upstreamServerAddresses);
+  upstreamServerAddresses = upstreamServerAddresses.filter(v => v.host && v.port)
+    .map(v => assign(v, defaultUpstreamInfo));
+  if (upstreamServerAddresses.length === 0) {
+    console.error('initUpstreamPool (upstreamServerAddresses.length === 0)');
+    throw new Error('initUpstreamPool (upstreamServerAddresses.length === 0)');
+  }
+}
 
 // This is where you pick which server to proxy to
 // for examples sake, I choose a random one
@@ -34,3 +55,22 @@ export function getServerBasedOnAddress(host: string | undefined) {
   return upstreamServerAddresses[Math.floor((Math.random() * 3))]
 }
 
+let socks5CheckTimer: Subscription | undefined = undefined;
+let connectCheckTimer: Subscription | undefined = undefined;
+
+export function startCheckTimer() {
+  if (socks5CheckTimer) {
+    socks5CheckTimer.unsubscribe();
+    socks5CheckTimer = undefined;
+  }
+  if (connectCheckTimer) {
+    connectCheckTimer.unsubscribe();
+    connectCheckTimer = undefined;
+  }
+  socks5CheckTimer = timer(100, 5 * 1000).subscribe(value => {
+
+  });
+  connectCheckTimer = timer(100, 5 * 60 * 1000).subscribe(value => {
+
+  });
+}
